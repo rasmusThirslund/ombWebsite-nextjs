45ed0b166386de2db5724a48eb5d9fa94e842017
Rasmus Thirslund <rasmus.thirslund@ombrello.io>
2021-11-05 16:02:27 +0100

    chore: clean up component code

--- src/pages/Typography/index.tsx
+++ src/pages/Typography/index.tsx
@@ -1,10 +1,6 @@
 import { HeadMeta } from "@app/components/layouts";
 import { content } from "../../constants/contentHtml.constants";
-import cx from "classnames";
 
-interface ContainerProps {
-  size?: "large";
-}
 export default function TypographyPage() {
   return (
     <>
