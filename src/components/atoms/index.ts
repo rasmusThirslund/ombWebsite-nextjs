0f6688e8e9c45a87e058dd4244dde79c6c537baa
Jacob Frederiksen <jacobfred88@gmail.com>
2021-11-12 10:07:55 +0100

    clean up

--- src/components/atoms/index.ts
+++ src/components/atoms/index.ts
@@ -1,12 +1,3 @@
 /* eslint-disable import/no-cycle */
-// export * from "./Button/Button";
-// export * from "./Image/Image";
-// export * from "./Link/Link";
-// export * from "./NewsCard/NewsCard";
-// export * from "./RichText/RichText";
-// export * from "./Typography/Typography";
-// export * from "./Meta/Meta";
-// export * from "./Icon/Icon";
 export * from "./Image/Image";
-// export * from "./RichText/RichText";
 export * from "./Debug/Debug";
